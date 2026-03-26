072a72fbd5f502749ce7e117519aefc510319488
Ebenzic-lang <adeboyeebenezer2000@gmail.com>
2026-03-26 13:19:52 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,74 @@
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import os
+
+BOT_TOKEN = os.getenv("8749400865:AAESYGArg4ODFr0sZYJW1jz1VHCamaza3Dc")
+
+CHANNEL_USERNAME = "@Vipwinningsignals"
+
+PLAY_LINK = os.getenv("https://t.me/Bybitglobal_Official_bot/app?startapp=redirect-Wednesday-Airdrop")
+OFFER_LINK = os.getenv("https://t.me/Bybitglobal_Official_bot/app?startapp=redirect-Wednesday-Airdrop")
+SUPPORT_LINK = os.getenv("t.me/Palmernews")
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+# START
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = InlineKeyboardMarkup()
+    markup.add(
+        InlineKeyboardButton("✅ Join Channel", url=f"https://t.me/{CHANNEL_USERNAME.replace('@','')}")
+    )
+    markup.add(
+        InlineKeyboardButton("🔓 I Joined (Unlock)", callback_data="check_join")
+    )
+
+    bot.send_message(
+        message.chat.id,
+        "Welcome 👋\n\nGet access to exclusive drops + winner alerts.\n\nStep 1/2: Join our channel to unlock.",
+        reply_markup=markup
+    )
+
+# CHECK JOIN
+@bot.callback_query_handler(func=lambda call: call.data == "check_join")
+def check_join(call):
+    user_id = call.from_user.id
+
+    try:
+        member = bot.get_chat_member(CHANNEL_USERNAME, user_id)
+
+        if member.status in ['member', 'administrator', 'creator']:
+            markup = InlineKeyboardMarkup()
+            markup.add(InlineKeyboardButton("🎰 Play Now", url=PLAY_LINK))
+            markup.add(InlineKeyboardButton("🎁 Today’s Offer", url=OFFER_LINK))
+            markup.add(InlineKeyboardButton("💬 Support", url=SUPPORT_LINK))
+
+            bot.edit_message_text(
+                "Unlocked 🎉\n\nStep 2/2: Continue below:",
+                call.message.chat.id,
+                call.message.message_id,
+                reply_markup=markup
+            )
+        else:
+            raise Exception("Not joined")
+
+    except:
+        markup = InlineKeyboardMarkup()
+        markup.add(
+            InlineKeyboardButton("✅ Join Channel", url=f"https://t.me/{CHANNEL_USERNAME.replace('@','')}")
+        )
+        markup.add(
+            InlineKeyboardButton("🔓 Try Unlock Again", callback_data="check_join")
+        )
+
+        bot.answer_callback_query(call.id, "You must join first!", show_alert=True)
+
+        bot.edit_message_text(
+            "Not subscribed yet—join to unlock access.\n\n• Exclusive signals\n• Daily winners\n• VIP alerts",
+            call.message.chat.id,
+            call.message.message_id,
+            reply_markup=markup
+        )
+
+print("Bot is running...")
+bot.infinity_polling()
